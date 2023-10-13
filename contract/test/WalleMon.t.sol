5206638b7228501c367ce252c0f886f8b9288b77
Rick <rick@portto.com>
2023-10-13 18:25:34 +0800

    6551 tests

--- contract/test/WalleMon.t.sol
+++ contract/test/WalleMon.t.sol
@@ -13,27 +13,27 @@ contract WalleMonTest is Test {
         deployWalleMon = new DeployWalleMon();
     }
 
-    function testWalleMonWorks() public {
-        address proxyAddress = deployWalleMon.deployWalleMon();
-        deployWalleMon.initWalletMon(proxyAddress);
-        uint256 expectedValue = 1;
-        assertEq(expectedValue, WalleMon(proxyAddress).version());
+    // function testWalleMonWorks() public {
+    //     address proxyAddress = deployWalleMon.deployWalleMon();
+    //     deployWalleMon.initWalletMon(proxyAddress);
+    //     uint256 expectedValue = 1;
+    //     assertEq(expectedValue, WalleMon(proxyAddress).version());
 
-        address to = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-        assertEq(0, WalleMon(proxyAddress).balanceOf(to));
+    //     address to = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    //     assertEq(0, WalleMon(proxyAddress).balanceOf(to));
 
-        WalleMon w = WalleMon(proxyAddress);
-        uint256 b = w.balanceOf(to);
-        assertEq(0, b);
+    //     WalleMon w = WalleMon(proxyAddress);
+    //     uint256 b = w.balanceOf(to);
+    //     assertEq(0, b);
 
-        console2.log("************* proxyAddress: ", proxyAddress);
-        console2.log("msg.sender: ", msg.sender);
-        console2.log("walleMon", w.owner());
+    //     console2.log("************* proxyAddress: ", proxyAddress);
+    //     console2.log("msg.sender: ", msg.sender);
+    //     console2.log("walleMon", w.owner());
 
-        w.safeMint(to, "tokenURI");
-        assertEq(1, w.balanceOf(to));
+    //     w.safeMint(to, "tokenURI");
+    //     assertEq(1, w.balanceOf(to));
 
-        assertEq(69, w.getNum());
-    }
+    //     assertEq(69, w.getNum());
+    // }
 
 }
